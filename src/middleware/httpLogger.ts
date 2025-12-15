8081dfb27afeac0aa4e6257d88a1aa565bd1c5c2
Prakhar Dabkara <prakhar.dabkara@fiftyfivetech.io>
2025-12-15 13:07:46 +0530

    Final Commit before dockerization

--- src/middleware/httpLogger.ts
+++ src/middleware/httpLogger.ts
@@ -4,12 +4,16 @@ import { randomUUID } from 'crypto';
 
 const httpLogger = pinoHttp({
   logger,
+
   genReqId: (req) => {
-    return req.headers['x-request-id'] as string || randomUUID();
+    return (req.headers['x-request-id'] as string) || randomUUID();
   },
+
   customLogLevel: (res, err) => {
-    if (res.statusCode >= 500 || err) return 'error';
-    if (res.statusCode >= 400) return 'warn';
+    const statusCode = res.statusCode ?? 500;
+
+    if (statusCode >= 500 || err) return 'error';
+    if (statusCode >= 400) return 'warn';
     return 'info';
   },
 });
